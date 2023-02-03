bf00d0531d2d9b8d567f4716910b76cda68566b9
bolt3x <valeriofassi@hotmail.it>
2023-02-04 00:35:12 +0100

    modified chebyshev test file

--- src/test_cases/chebyshev.cpp
+++ src/test_cases/chebyshev.cpp
@@ -3,10 +3,10 @@
 #include "Krylov.hpp"
 
 int main(){
- std::cout << "Chebyshev" << std::endl;
- Krylov::SparseMatrix<double> A = Krylov::readCSRMatrix<double>("../matrixes/bcsstm22.mtx");
- 
- double n = A.cols();                                                                                                                                                                        
+ std::cout << "Chebyshev" << std::endl;     
+ Krylov::SparseMatrix<double> A = Krylov::readCSRMatrix<double>("../matrixes/diffreact.mtx");
+
+ double n = A.cols();
  Krylov::Vector<double> xe(n,1);
  Krylov::Vector<double> x(n);
 
@@ -21,18 +21,18 @@ int main(){
  double a_max;
  double a_min;
 
- max_iter = 2000;
+ max_iter = 1000;
  tol = 1e-15;
  Krylov::Vector<double> v_max(n,1);
  res = Power(A,a_max,v_max,max_iter,tol);
 
- max_iter = 2000;
+ max_iter = 1000;
  tol = 1e-15;
  Krylov::Vector<double> v_min(n,1);
  res = InversePower(A,a_min,v_min,Krylov::CG,id,max_iter,tol);
  
  tol = 1e-9;
- max_iter = 2000;
+ max_iter = 1000;
  res = Chebyshev(A,x,b,id,max_iter,tol,a_min,a_max);
 
  std::cout << std::endl << "-------------------------" << std::endl;
@@ -42,6 +42,10 @@ int main(){
  std::cout << "Residual: " << tol << std::endl;
  std::cout << "Error norm: " << (x-xe).norm() << std::endl;
  std::cout << "-------------------------" << std::endl;
+
+ res = Krylov::plotCSRMatrix<double>(A,"original");
+ res = Krylov::plotCSRMatrix<double>(spaiD.getM(),"dynamic");
+ res = Krylov::plotCSRMatrix<double>(spaiS.getM(),"static");
 
  
 }
